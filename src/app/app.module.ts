077b6905643d7cd1f22bd23fe8bf2e252bb76614
Mahesh Cheemalapati <mcheemalapati@gmail.com>
2022-03-16 11:06:40 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CardComponent } from './card/card.component';
+import { CardTabsComponent } from './card-tabs/card-tabs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardTabsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/card-tabs/card-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-tabs',
+  templateUrl: './card-tabs.component.html',
+  styleUrls: ['./card-tabs.component.scss']
+})
+export class CardTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
